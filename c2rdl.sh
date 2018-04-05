31c848db8400f89b034f6f993b4d250ecd74453f
CNMan <nklijl@mail.nankai.edu.cn>
2018-04-05 04:43:15 +0000

    add AudienceId

--- c2rdl.sh
+++ c2rdl.sh
@@ -28,31 +28,31 @@ cd /root/C2R/ProductionLTSC && cabextract v64.cab
 
 echo '# C2R' >/root/C2R/README.MD
 echo ' ' >>/root/C2R/README.MD
-echo 'DogfoodCC' >>/root/C2R/README.MD
+echo 'DogfoodCC F3260CF1-A92C-4C75-B02E-D64C0A86A968' >>/root/C2R/README.MD
 cd /root/C2R/DogfoodCC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'DogfoodDevMain' >>/root/C2R/README.MD
+echo 'DogfoodDevMain EA4A4090-DE26-49D7-93C1-91BFF9E53FC3' >>/root/C2R/README.MD
 cd /root/C2R/DogfoodDevMain && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'DogfoodFRDC' >>/root/C2R/README.MD
+echo 'DogfoodFRDC 834504CC-DC55-4C6D-9E71-E024D0253F6D' >>/root/C2R/README.MD
 cd /root/C2R/DogfoodFRDC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'InsidersCC' >>/root/C2R/README.MD
+echo 'InsidersCC 64256AFE-F5D9-4F86-8936-8840A6A4F5BE' >>/root/C2R/README.MD
 cd /root/C2R/InsidersCC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'InsidersDevMain' >>/root/C2R/README.MD
+echo 'InsidersDevMain 5440FD1F-7ECB-4221-8110-145EFAA6372F' >>/root/C2R/README.MD
 cd /root/C2R/InsidersDevMain && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'InsidersFRDC' >>/root/C2R/README.MD
+echo 'InsidersFRDC B8F9B850-328D-4355-9145-C59439A0C4CF' >>/root/C2R/README.MD
 cd /root/C2R/InsidersFRDC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'MicrosoftCC' >>/root/C2R/README.MD
+echo 'MicrosoftCC 5462EEE5-1E97-495B-9370-853CD873BB07' >>/root/C2R/README.MD
 cd /root/C2R/MicrosoftCC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'MicrosoftDC' >>/root/C2R/README.MD
+echo 'MicrosoftDC F4F024C8-D611-4748-A7E0-02B6E754C0FE' >>/root/C2R/README.MD
 cd /root/C2R/MicrosoftDC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'MicrosoftDevMain' >>/root/C2R/README.MD
+echo 'MicrosoftDevMain B61285DD-D9F7-41F2-9757-8F61CBA4E9C8' >>/root/C2R/README.MD
 cd /root/C2R/MicrosoftDevMain && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'MicrosoftFRDC' >>/root/C2R/README.MD
+echo 'MicrosoftFRDC 9A3B7FF2-58ED-40FD-ADD5-1E5158059D1C' >>/root/C2R/README.MD
 cd /root/C2R/MicrosoftFRDC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'ProductionCC' >>/root/C2R/README.MD
+echo 'ProductionCC 492350F6-3A01-4F97-B9C0-C7C6DDF67D60' >>/root/C2R/README.MD
 cd /root/C2R/ProductionCC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'ProductionDC' >>/root/C2R/README.MD
+echo 'ProductionDC 7FFBC6BF-BC32-4F92-8982-F9DD17FD3114' >>/root/C2R/README.MD
 cd /root/C2R/ProductionDC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
-echo 'ProductionLTSC' >>/root/C2R/README.MD
+echo 'ProductionLTSC F2E724C1-748F-4B47-8FB8-8E0D210E9208' >>/root/C2R/README.MD
 cd /root/C2R/ProductionLTSC && awk 'NR==2{print}' v64.hash >>/root/C2R/README.MD
 
 cd /root/C2R
