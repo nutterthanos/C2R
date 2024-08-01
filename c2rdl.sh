cdf5cbdb35281f56251cea8c68b576bb918e2a34
nutterthanos <minecraftitsover90@gmail.com>
2024-08-01 14:56:32 +0930

    Update c2rdl.sh

--- c2rdl.sh
+++ c2rdl.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-find . -mindepth 1 ! -name 'c2rdl.sh' ! -name '.github' ! -name '.git' -print0 | xargs -0 echo
+find . -mindepth 1 | grep -v -f keep.txt
 
 URL=officecdn.microsoft.com/sg
 DIR=Office/Data
